e230b3b762603f70a1ff21f0c8ade630eeb4a51d
liujiaoteng <962905977@qq.com>
2019-05-19 20:46:48 +0800

    LJT

--- src/main/webapp/pages/js/util.js
+++ src/main/webapp/pages/js/util.js
@@ -6,15 +6,3 @@ function isEmpty(str) {
 function jsTrim(x) {
 	return x.replace(/^\s+|\s+$/gm, '');
 }
-
-<script>
-//用于去除alert的url
-window.alert = function(name){
-    var iframe = document.createElement("IFRAME");
-    iframe.style.display="none";
-    iframe.setAttribute("src", 'data:text/plain,');
-    document.documentElement.appendChild(iframe);
-    window.frames[0].window.alert(name);
-    iframe.parentNode.removeChild(iframe);
-}
-</script>
