c3259e522b9b4bce08c4518630433e73092eaac6
liujiaoteng <962905977@qq.com>
2019-05-20 17:01:39 +0800

    LJT
    交易记录列表

--- src/main/webapp/pages/js/util.js
+++ src/main/webapp/pages/js/util.js
@@ -1,8 +1,33 @@
 function isEmpty(str) {
 	return str == null || str == undefined || str == ""
-			|| jsTrim(str).length == 0;
+			|| str.length == 0;
 }
 
-function jsTrim(x) {
-	return x.replace(/^\s+|\s+$/gm, '');
+String.prototype.format = function (args) {
+    var result = this;
+    if (arguments.length < 1) {
+        return result;
+    }
+
+    var data = arguments;       //如果模板参数是数组
+    if (arguments.length == 1 && typeof (args) == "object") {
+        //如果模板参数是对象
+        data = args;
+    }
+    for (var key in data) {
+        var value = data[key];
+        if (!isEmpty(value)) {
+            result = result.replace(new RegExp("{" + key + "}", 'g'), value);
+        }else{
+        	
+        	 result = result.replace(new RegExp("{" + key + "}", 'g'), "");
+        }
+    }
+    return result;
 }
+
+
+function jsTrim(x) {
+	return x.replace(new RegExp("/^\s+|\s+$/gm"),'');
+}
+
